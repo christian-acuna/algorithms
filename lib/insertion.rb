f7a4b147b2bc1afe5cf519888f94309ab2474db4
Christian Acuna <cacuna0828@gmail.com>
2016-08-20 09:51:11 +0800

    Implement insertion sort for

---
+++ lib/insertion.rb
@@ -0,0 +1,31 @@
+class Insertion
+  def sort(array)
+    current_element_index = 1
+    while current_element_index < array.length
+      key = array[current_element_index]
+      i = current_element_index - 1
+      while i >= 0 && array[i] > key
+        array[i + 1] = array[i]
+        i -= 1
+      end
+      array[i + 1] = key
+      current_element_index += 1
+    end
+    array
+  end
+
+  def nonincreasing(array)
+    current_element_index = 1
+    while current_element_index < array.length
+      key = array[current_element_index]
+      i = current_element_index - 1
+      while i >= 0 && array[i] < key
+        array[i + 1] = array[i]
+        i -= 1
+      end
+      array[i + 1] = key
+      current_element_index += 1
+    end
+    array
+  end
+end
---
+++ spec/insertion_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe Insertion do
+  context 'non-decreasing' do
+    let(:insert) { Insertion.new }
+    it 'sorts a one element array' do
+      expect(insert.sort([1])).to eq [1]
+    end
+
+    it 'sorts a two element array' do
+      expect(insert.sort([2, 1])).to eq [1, 2]
+    end
+
+    it 'sorts a three element array' do
+      expect(insert.sort([3, 2, 1])).to eq [1, 2, 3]
+    end
+
+    it 'sorts a five element array' do
+      expect(insert.sort([5, 1, 4, 2, 8])).to eq [1, 2, 4, 5, 8]
+    end
+
+    it 'sorts a six element array' do
+      expect(insert.sort([5, 2, 4, 6, 1, 3])).to eq [1, 2, 3, 4, 5, 6]
+    end
+  end
+
+  context 'nonincreasing' do
+    let(:insert) { Insertion.new }
+    it 'sorts a one element array' do
+      expect(insert.nonincreasing([1])).to eq [1]
+    end
+
+    it 'sorts a two element array' do
+      expect(insert.nonincreasing([1, 2])).to eq [2, 1]
+    end
+
+    it 'sorts a three element array' do
+      expect(insert.nonincreasing([1, 2, 3])).to eq [3, 2, 1]
+    end
+
+    it 'sorts a five element array' do
+      expect(insert.nonincreasing([5, 1, 4, 2, 8])).to eq [8, 5, 4, 2, 1]
+    end
+
+    it 'sorts a six element array' do
+      expect(insert.nonincreasing([5, 2, 4, 6, 1, 3])).to eq [6, 5, 4, 3, 2, 1]
+    end
+  end
+end
